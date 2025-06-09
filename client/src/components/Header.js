f20da5be7435b240d8e2b2404653d7e8860609fc
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-06-09 14:03:08 +0300

    Add new design for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
 
 export default function Header() {
     const { admin } = useAuth();
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     const loggedAdmin = (
         <>
@@ -10,17 +16,21 @@ export default function Header() {
             <Link to="/admin/logout">Изход</Link>
         </>
     );
+
     return (
         <>
             <div className="navbar">
-                <Link to="/Предястия">Предястия</Link>
-                <Link to="/Основни">Основни</Link>
-                <Link to="/Десерти">Десерти</Link>
-                <Link to="/Салати">Салати</Link>
-                <Link to="/Напитки">Напитки</Link>
-                {
-                    admin.email ? loggedAdmin : ''
-                }
+                <button className="menu-toggle" onClick={toggleMenu}>
+                    ☰
+                </button>
+                <div className={`menu-links ${menuOpen ? "open" : ""}`}>
+                    <Link to="/Предястия">Предястия</Link>
+                    <Link to="/Основни">Основни</Link>
+                    <Link to="/Десерти">Десерти</Link>
+                    <Link to="/Салати">Салати</Link>
+                    <Link to="/Напитки">Напитки</Link>
+                    {admin.email ? loggedAdmin : ""}
+                </div>
             </div>
         </>
     );
