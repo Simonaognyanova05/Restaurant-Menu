b22eee9f34e4c443496d1a9a5ce5039ea5fec189
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-09-10 10:47:29 +0300

    Add header for phone

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,39 +10,41 @@ export default function Header() {
         setMenuOpen(!menuOpen);
     };
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     const loggedAdmin = (
         <>
-            <Link to="/admin/create">Създаване на ястие</Link>
-            <Link to="/admin/register">Създаване на потребител</Link>
-            <Link to="/admin/logout">Изход</Link>
+            <Link to="/admin/create" onClick={closeMenu}>Създаване на ястие</Link>
+            <Link to="/admin/register" onClick={closeMenu}>Създаване на потребител</Link>
+            <Link to="/admin/logout" onClick={closeMenu}>Изход</Link>
         </>
     );
 
     return (
-        <>
-            <div className="navbar">
-                <button className="menu-toggle" onClick={toggleMenu}>
-                    ☰
-                </button>
-                <div className={`menu-links ${menuOpen ? "open" : ""}`}>
-                    <Link to="/Закуски">Закуски</Link>
-                    <Link to="/Топли-предястия">Топли предястия</Link>
-                    <Link to="/Супи">Супи</Link>
-                    <Link to="/Основни">Основни</Link>
-                    <Link to="/Скара">Скара</Link>
-                    <Link to="/Риба">Риба</Link>
-                    <Link to="/Гарнитура">Гарнитура</Link>
-                    <Link to="/Салати">Салати</Link>
-                    <Link to="/Хляб">Хляб</Link>
-                    <Link to="/Десерти">Десерти</Link>
-                    <Link to="/Мезета">Мезета</Link>
-                    <Link to="/Ядки">Ядки</Link>
-                    <Link to="/Напитки">Напитки</Link>
-                    <Link to="/Безалкохолни">Безалкохолни</Link>
+        <div className="navbar">
+            <button className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </button>
+            <div className={`menu-links ${menuOpen ? "open" : ""}`}>
+                <Link to="/Закуски" onClick={closeMenu}>Закуски</Link>
+                <Link to="/Топли-предястия" onClick={closeMenu}>Топли предястия</Link>
+                <Link to="/Супи" onClick={closeMenu}>Супи</Link>
+                <Link to="/Основни" onClick={closeMenu}>Основни</Link>
+                <Link to="/Скара" onClick={closeMenu}>Скара</Link>
+                <Link to="/Риба" onClick={closeMenu}>Риба</Link>
+                <Link to="/Гарнитура" onClick={closeMenu}>Гарнитура</Link>
+                <Link to="/Салати" onClick={closeMenu}>Салати</Link>
+                <Link to="/Хляб" onClick={closeMenu}>Хляб</Link>
+                <Link to="/Десерти" onClick={closeMenu}>Десерти</Link>
+                <Link to="/Мезета" onClick={closeMenu}>Мезета</Link>
+                <Link to="/Ядки" onClick={closeMenu}>Ядки</Link>
+                <Link to="/Напитки" onClick={closeMenu}>Напитки</Link>
+                <Link to="/Безалкохолни" onClick={closeMenu}>Безалкохолни</Link>
 
-                    {admin.email ? loggedAdmin : ""}
-                </div>
+                {admin.email ? loggedAdmin : ""}
             </div>
-        </>
+        </div>
     );
 }
